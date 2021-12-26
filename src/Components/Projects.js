6509743b881351a8661ab030828f0bfb6e753e02
Guilherme <guilhermenardi3@outlook.com>
2021-12-26 01:15:08 -0300

    add projeto

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -10,7 +10,7 @@ class Projects extends React.Component {
                     <div className='projects'>
                         <a href="https://nardi00.github.io/react-weather-app/" className='project-title anchor'>· Aplicativo de temperatura</a>
                         <p className='project-description'>aplicativo React que mostra a temperatura no seu local através da geolocalização do browser - <span><a href='https://github.com/nardi00/react-weather-app'>github</a></span></p>
-                        
+
                         <a href="https://nardi00.github.io/news/" className='project-title anchor'>· Aplicativo de notícias</a>
                         <p className='project-description'>aplicativo React que apresenta os novos artigos mais relevantes do New York Times e possibilita a procura de outros artigos pelo usuário - <span><a href='https://github.com/nardi00/news'>github</a></span></p>
                     </div>
